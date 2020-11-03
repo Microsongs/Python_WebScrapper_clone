9fe0232fc73cf5fdd9c84a9255c2d93143e9afcd
현석 송 <jhn06046@hotmail.com>
2020-11-03 14:31:51 +0900

    1장 Theory 완강

---
+++ 1.Theory/ForIn.py
@@ -0,0 +1,7 @@
+days = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+
+for day in days:
+    if day in "Wed":
+        break
+    else:
+        print(day)
---
+++ 1.Theory/Modules.py
@@ -0,0 +1,9 @@
+from math import fsum as mySum
+from math import ceil
+from math import fsum
+from Modules_calculator import plus, minus
+
+print(ceil(1.2))
+print(fsum([1,2,3,4,5,6,7]))
+print(mySum([1,2,3,4]))
+print(plus(2,3), minus(1,2))
